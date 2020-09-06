5d4b778a588d8e0beb0da5e5d2fbf6af844f5a1a
yty2006 <yty_061202@163.com>
2020-09-06 17:37:46 +0800

    Site updated: 2020-09-06 17:37:45

---
+++ music.js
@@ -0,0 +1,9 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: 'Revival',
+        artist: 'Ahxello',
+        url: '/revival.mp3',
+        cover: 'https://pic.xiami.net/images/album/imgs/405/640/a9fc1e64_1282572405_1513657710463.jpg?x-oss-process=image/resize,limit_0,m_fill,s_410/quality,q_80/format,jpg'
+    }]
+});
